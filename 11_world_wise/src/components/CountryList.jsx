4916e231e8db052f77a75f30988ecc19b53f0292
Adriano Chamon <adrianochamontavares@gmail.com>
2025-03-25 17:25:24 -0300

    feat: finished section, will return to this project, next step: context api

---
+++ 11_world_wise/src/components/CountryList.jsx
@@ -0,0 +1,37 @@
+import styles from "./CountryList.module.css";
+import Spinner from "./Spinner";
+import CountryItem from "./CountryItem";
+import PropTypes from "prop-types";
+import Message from "./Message";
+
+function CountryList({ cities, isLoading }) {
+  if (isLoading) return <Spinner />;
+
+  if (!cities.length)
+    return (
+      <Message message="Add your first city by clicking on a city on the map" />
+    );
+
+  const countries = cities.reduce((acc, city) => {
+    if (!acc.map((el) => el.country).includes(city.country)) {
+      return [...acc, { country: city.country, emoji: city.emoji }];
+    } else {
+      return acc;
+    }
+  }, []);
+
+  return (
+    <ul className={styles.countryList}>
+      {countries.map((country) => (
+        <CountryItem country={country} key={country.country} />
+      ))}
+    </ul>
+  );
+}
+
+CountryList.propTypes = {
+  cities: PropTypes.arrayOf(PropTypes.object).isRequired,
+  isLoading: PropTypes.bool.isRequired,
+};
+
+export default CountryList;
