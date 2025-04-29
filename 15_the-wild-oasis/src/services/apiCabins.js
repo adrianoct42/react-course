91140faf1475a33acc538d9bb2eb6ab5fd054eed
Adriano Chamon <adrianochamontavares@gmail.com>
2025-04-28 22:55:51 -0300

    feat: new API integration in wild oasis

--- 15_the-wild-oasis/src/services/apiCabins.js
+++ 15_the-wild-oasis/src/services/apiCabins.js
@@ -1,4 +1,4 @@
-import supabase from "./supabase";
+import supabase, { supabaseUrl } from "./supabase";
 
 export async function getCabins() {
   let { data, error } = await supabase.from("cabins").select("*");
@@ -6,6 +6,67 @@ export async function getCabins() {
   if (error) {
     console.log(error);
     throw new Error("Cabin could not be loaded");
+  }
+
+  return data;
+}
+
+/* 
+  O motivo pelo qual podemos passar "newCabin" abaixo no insert sem ter os nomes dos campos
+  escritos, é porque no CreateCabinForm, estamos registrando o register com nomes iguais
+  aos nomes dos campos no banco do supabase.
+
+  Do contrário, o insert deveria ter a seguinte estrutura:
+
+    .insert([
+      { some_column: 'someValue', other_column: 'otherValue' },
+    ])
+*/
+
+// Funciona tanto para CRIAR e EDITAR!
+export async function createEditCabin(newCabin, id) {
+  const hasImagePath = newCabin.image?.startsWith?.(supabaseUrl);
+
+  const imageName = `${Math.random()}-${newCabin.image.name}`.replaceAll(
+    "/",
+    ""
+  );
+  const imagePath = hasImagePath
+    ? newCabin.image
+    : `${supabaseUrl}/storage/v1/object/public/cabin-images/${imageName}`;
+
+  // 1. Create/Edit cabin
+  let query = supabase.from("cabins");
+
+  // 1- a) CREATE
+  if (!id) {
+    query = query.insert([{ ...newCabin, image: imagePath }]);
+  }
+
+  // 1- b) EDIT
+  if (id) {
+    query = query.update({ ...newCabin, image: imagePath }).eq("id", id);
+  }
+
+  const { data, error } = await query.select().single();
+
+  if (error) {
+    console.log(error);
+    throw new Error("Cabin could not be created");
+  }
+
+  // 2. Upload image
+  const { error: storageError } = await supabase.storage
+    .from("cabin-images")
+    .upload(imageName, newCabin.image);
+
+  // 3. Delete the cabin IF the was an error uploading the image
+  if (storageError) {
+    await supabase.from("cabins").delete().eq("id", data.id);
+    console.log(storageError);
+    throw new Error(
+      "Cabin image could not be uploaded and the cabin was not created"
+    );
   }
 
   return data;
--- 15_the-wild-oasis/src/services/supabase.js
+++ 15_the-wild-oasis/src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://bgrllwltcmfizkfnmbzj.supabase.co";
+export const supabaseUrl = "https://bgrllwltcmfizkfnmbzj.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJncmxsd2x0Y21maXprZm5tYnpqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ5MTcyNjUsImV4cCI6MjA2MDQ5MzI2NX0.hnS5uDqGc9YhZMZ1aXQGXq2doC1j0d7Uon2NKKAr46U";
 const supabase = createClient(supabaseUrl, supabaseKey);
