099cc8c94b08a71c040be4ed586534e99cbcaf7f
Adriano Chamon <adrianochamontavares@gmail.com>
2025-05-19 21:10:45 -0300

    feat: started nextJs project

---
+++ 17_the-wild-oasis-website/app/layout.js
@@ -0,0 +1,33 @@
+import "@/app/_styles/globals.css";
+
+import { Josefin_Sans } from "next/font/google";
+import Header from "./_components/Header";
+
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const metadata = {
+  title: {
+    template: "%s / The Wild Oasis",
+    default: "Welcome / to The Wild Oasis",
+  },
+  description:
+    "Luxurious cabin hotel, located in the heart of the Italian Dolomites, surrounded by beautiful mountains and dark forests",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${josefin.className} antialiased min-h-screen bg-primary-950 text-primary-100 flex flex-col relative`}
+      >
+        <Header />
+        <div className="grid flex-1 px-8 py-12">
+          <main className="w-full mx-auto max-w-7xl">{children}</main>
+        </div>
+      </body>
+    </html>
+  );
+}
